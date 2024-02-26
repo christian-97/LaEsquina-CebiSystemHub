f7743072f8df78c143281d33b6253e3ff9e35121
CGR <CGR@DESKTOP-759SE1L>
2024-02-25 22:35:17 -0500

    cors 1

--- src/main/java/com/LaEsquina/CebiSystemHub/config/CorsConfig.java
+++ src/main/java/com/LaEsquina/CebiSystemHub/config/CorsConfig.java
@@ -1,8 +1,13 @@
 package com.LaEsquina.CebiSystemHub.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer{
 	
 	public void addCorsMapping(CorsRegistry registrer) {
