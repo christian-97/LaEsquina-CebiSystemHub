814fc1198d6096c0c4815494c89825d42f048fc5
CGR <CGR@DESKTOP-759SE1L>
2024-02-25 23:02:16 -0500

    bean1

--- src/main/java/com/LaEsquina/CebiSystemHub/Controller/corsConfigurerbean.java
+++ src/main/java/com/LaEsquina/CebiSystemHub/Controller/corsConfigurerbean.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class corsConfigurer {
+public class corsConfigurerbean {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
