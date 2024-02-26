14abe80c4f16c0aeb221e36e57b88b7a74d86bea
CGR <CGR@DESKTOP-759SE1L>
2024-02-25 22:41:03 -0500

    cors config 3

--- src/main/java/com/LaEsquina/CebiSystemHub/Controller/UsuarioController.java
+++ src/main/java/com/LaEsquina/CebiSystemHub/Controller/UsuarioController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin(origins = { "*" })
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class UsuarioController {
 
     @Autowired
