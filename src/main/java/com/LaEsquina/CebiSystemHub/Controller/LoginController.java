9d87571a62d874885f206830e2b4d3601538ce88
CGR <CGR@DESKTOP-759SE1L>
2024-02-25 22:10:06 -0500

    CORS

--- src/main/java/com/LaEsquina/CebiSystemHub/Controller/LoginController.java
+++ src/main/java/com/LaEsquina/CebiSystemHub/Controller/LoginController.java
@@ -3,6 +3,7 @@ package com.LaEsquina.CebiSystemHub.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.LaEsquina.CebiSystemHub.Service.LoginRequestService;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = { "*" })
 public class LoginController {
 
     @Autowired
