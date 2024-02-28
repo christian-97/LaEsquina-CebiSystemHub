5d5eec8d73ff754599089788d8ef274d2ec90a4b
christian-97 <60308019+christian-97@users.noreply.github.com>
2024-02-27 21:32:39 -0500

    correcciones

--- src/main/java/com/LaEsquina/CebiSystemHub/Controller/corsConfigurerbean.java
+++ src/main/java/com/LaEsquina/CebiSystemHub/Controller/corsConfigurerbean.java
@@ -14,7 +14,7 @@ public class corsConfigurerbean {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") 
+                        .allowedOrigins("http://127.0.0.1:5500") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE") 
                         .allowedHeaders("Content-Type", "Authorization")
                         .allowCredentials(true) 
